556f52b737318461626804f18a9995fb81e2fb52
igp-gravity <chenshi@cea-igp.ac.cn>
2017-12-31 22:39:10 +0800

    3

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # 测试工程 documentation build configuration file, created by
@@ -46,9 +46,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = '测试工程'
-copyright = '2017, 我'
-author = '我'
+project = 'test prj'
+copyright = '201711'
+author = 'i'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -64,7 +64,7 @@ release = '1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = '中文'
+language = 'CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -139,8 +139,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sphinx.tex', '测试工程 Documentation',
-     '我', 'manual'),
+    (master_doc, 'sphinx.tex', 'test Documentation',
+     'i', 'manual'),
 ]
 
 
@@ -149,7 +149,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sphinx', '测试工程 Documentation',
+    (master_doc, 'sphinx', 'test Documentation',
      [author], 1)
 ]
 
@@ -160,7 +160,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sphinx', '测试工程 Documentation',
+    (master_doc, 'sphinx', 'test Documentation',
      author, 'sphinx', 'One line description of project.',
      'Miscellaneous'),
 ]
