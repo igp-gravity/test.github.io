51c61fc0f225bac03d0974eccfda10e95daf714d
igp-gravity <chenshi@cea-igp.ac.cn>
2018-01-06 13:59:06 +0800

    0

--- conf.py
+++ conf.py
@@ -116,7 +116,6 @@ htmlhelp_basename = 'sphinxdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-latex_engine = 'xelatex'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
